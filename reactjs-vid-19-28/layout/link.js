fda4734eab81136d3e738ce0647554068f6fd0fc
Muhammad Falah <110474441+MuhammadFalahKresnatra@users.noreply.github.com>
2023-02-12 11:36:35 +0700

    Delete reactjs-vid-19-28 directory

--- reactjs-vid-19-28/layout/link.js
+++
@@ -1,9 +0,0 @@
-const url = "http://localhost:8000/api";
-        let token = "7hQi6p7wQk9qT1mdDihFw8lmaDwxIWrU9PQbwkvl";
-
-        export const link = axios.create({
-            baseURL : url,
-            headers : {
-                'api_token' : token
-            }
-        });
