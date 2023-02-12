6e78247da75ecb90904ebdbfa5d18669498fa9f6
MuhammadFalahKresnatra <110474441+MuhammadFalahKresnatra@users.noreply.github.com>
2023-02-12 11:33:16 +0700

    Vid 19-28

---
+++ reactjs-vid-19-28/layout/link.js
@@ -0,0 +1,9 @@
+const url = "http://localhost:8000/api";
+        let token = "7hQi6p7wQk9qT1mdDihFw8lmaDwxIWrU9PQbwkvl";
+
+        export const link = axios.create({
+            baseURL : url,
+            headers : {
+                'api_token' : token
+            }
+        });
